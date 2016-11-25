801fecbc7b0b42bb51ea7bacc9df2b9a7a43e181
shereen messi <shereenmessi@hotmail.com>
2016-11-25 20:41:57 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,7 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  def build_network(call_letters:)
+    self.network=Network.new(call_letters: call_letters)
+  end
 end
